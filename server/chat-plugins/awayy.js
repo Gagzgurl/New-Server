492ce3a43a28f8a192b34d23e3b7c6aa0d812107
Gaga <49109890+Gagzgurl@users.noreply.github.com>
2019-04-01 05:38:48 +0200

    Rename away.js to awayy.js

---
+++ server/chat-plugins/awayy.js
@@ -0,0 +1,7 @@
+b=m(b,c,d,e,g[f+12],6,1700485571),e=m(e,b,c,d,g[f+3],10,2399980690),d=m(d,e,b,c,g[f+10],15,4293915773),c=m(c,d,e,b,g[f+1],21,2240044497),b=m(b,c,d,e,g[f+8],6,1873313359),e=m(e,b,c,d,g[f+15],10,4264355552),d=m(d,e,b,c,g[f+6],15,2734768916),c=m(c,d,e,b,g[f+13],21,1309151649),b=m(b,c,d,e,g[f+4],6,4149444226),e=m(e,b,c,d,g[f+11],10,3174756917),d=m(d,e,b,c,g[f+2],15,718787259),c=m(c,d,e,b,g[f+9],21,3951481745),b=i(b,o),c=i(c,p),d=i(d,q),e=i(e,r);return(n(b)+n(c)+n(d)+n(e)).toLowerCase()};
+
+function hashColor(name) {
+	var hash;
+	hash = MD5(name);
+	var H = parseInt(hash.substr(4, 4), 16) % 360;
+if (user.can('lock', null, room)) this.add('|raw|-- <font color="' + hashColor(user.userid) + '"><strong>' + Tools.escapeHTML(user.name) + '</strong></font> ahora está ' + target.toLowerCase() + '.');
